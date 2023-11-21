2c410246983511a468b99fb196f83ec593aa8ccd
Shashank-k12 <151531682+Shashank-k12@users.noreply.github.com>
2023-11-21 17:45:23 +0500

    First day problems shashank (#2)
    * first day
    
    * voting
    
    * height
    
    * odd or even

---
+++ practice/first day/shashank_problems/problem1.js
@@ -0,0 +1,4 @@
+//check wheather person is eligible for voting
+if ("age of a person is <= 18") console.log("person is eligble for voting");
+else  ("age of a person is >=19");
+console.log("person is not eligible for voting");
---
+++ practice/first day/shashank_problems/problem2.js
@@ -0,0 +1,9 @@
+//to find the height of a person and cateogry according to their height
+if ("height of a person is > 150") console.log("person is dwaf");
+ifelse ("height of a person is = 150") console.log("person is average height");
+else ("height of a person is > 150") console.log("person is taller")
+if ("person height is <150") console.log("person is third preference")
+else ("person height is = 150")console.log("person is second preference")
+else("person height is > 150") console.log("person is first preference")
+
+)
