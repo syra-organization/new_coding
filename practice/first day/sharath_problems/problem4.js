a74f4113fdcdc32ffc2ee74634068fc24fd1d0d4
Sharath-new <151363492+Sharath-new@users.noreply.github.com>
2023-11-21 13:35:04 +0530

    problem4.js

---
+++ practice/first day/sharath_problems/problem4.js
@@ -0,0 +1,9 @@
+var n = 30;
+var m = 26;
+var num;
+if (n % 3 == 0) {
+  console.log("given number is even  and number is divisible by 3  ");
+} else m % 3 == 0;
+{
+  console.log("given number is odd  ");
+}
