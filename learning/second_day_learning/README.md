572f2fe1ad427f2f3f8c8ebb3751f8ccfcaf7527
Avinash K <k3076183@gmail.com>
2023-11-24 18:26:59 +0530

    Add loops probems

---
+++ learning/second_day_learning/README.md
@@ -0,0 +1,16 @@
+# Loops
+
+### While loops
+
+```
+syntax
+
+initiazation
+
+while (condition) {
+    // coding
+    it can be any coding
+
+    incrementing or decrementing
+}
+```
---
+++ learning/second_day_learning/whileloop.js
@@ -0,0 +1,54 @@
+// Print 1 to given number
+
+// function foo(num) {
+//   var i = 1;
+
+//   while (i <= num) {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+// foo(10)
+
+// function reverLoop(num) {
+//   var i = num;
+
+//   while (i >= 1) {
+//     console.log(i);
+//     i--;
+//   }
+// }
+
+// reverLoop(5)
+
+// right loop which will starts from one and always increment by one and given a number
+// your while loop should stop at that number
+
+// function stopAt(num){
+
+//     var i = 1;
+
+//     while(i < 10){
+//         console.log(i)
+//         i++
+//     }
+// }
+
+// count the number of even numbers between 1 to num
+
+// function counting(num) {
+//   var i = 1;
+//   var count = 0;
+//   while (i <= num) {
+//     //coding
+//     if (i % 2 != 0) {
+//       count++;
+//     }
+//     //increment
+//     i++;
+//   }
+//   console.log(count)
+// }
+
+// counting(6)
