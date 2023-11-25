6446730fa309e1de6b099e3ada76381287c00f37
Avinash K <k3076183@gmail.com>
2023-11-25 15:05:23 +0530

    Add second problem

---
+++ practice/second day/sharath_problems/loops/problem2.js
@@ -0,0 +1,10 @@
+// given a number n you need to print from 1 to n in a single line
+
+// ex: let say number is 5
+// output: 1 2 3 4 5
+
+function printInSingleLine(n) {
+    
+}
+
+printInSingleLine(5);
