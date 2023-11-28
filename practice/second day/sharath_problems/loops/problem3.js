0ff93ce09d7bea272d5d26385f73e74bf82be955
Sharath-new <151363492+Sharath-new@users.noreply.github.com>
2023-11-28 12:14:18 +0530

    sharath-problem3

--- practice/second day/sharath_problems/loops/problem3.js
+++ practice/second day/sharath_problems/loops/problem3.js
@@ -1,10 +1,18 @@
 // count the number of even numbers from 1 to given number
 
-// example: let say the given number is 10 
+// example: let say the given number is 10
 // the number of even numbers between 1 to 10 is 5
 
 function countEvenNumbers(num) {
-
+  var i = 1;
+  var count = 0;
+  while (i <= num) {
+    if (i % 2 == 0) {
+      count++;
+    }
+    i++;
+  }
+  console.log("given the even number", count);
 }
 
-countEvenNumbers(10)
+countEvenNumbers(10);
